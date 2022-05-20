3c877e27acc0cca1333e8096ea9916d76dd23aec
yanpengju <yanpengju@masastack.com>
2022-05-20 18:11:02 +0800

    feat: add config page

---
+++ src/Web/Masa.Dcc.Web.Admin/Masa.Dcc.Web.Admin.Rcl/Pages/Config.razor.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Dcc.Web.Admin.Rcl.Pages
+{
+    public partial class Config
+    {
+        [Parameter]
+        public Model.AppModel AppDetail { get; set; } = null!;
+
+        [Parameter]
+        public List<EnvironmentClusterModel> AppEnvClusters { get; set; } = null!;
+    }
+}

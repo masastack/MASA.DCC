ac76f75d68461038df026c24750dc2f7a637cfb5
yanpengju <yanpengju@masastack.com>
2022-10-26 17:39:21 +0800

    chore: remove using

--- src/Web/Masa.Dcc.Web.Admin/Masa.Dcc.Web.Admin.Server/Program.cs
+++ src/Web/Masa.Dcc.Web.Admin/Masa.Dcc.Web.Admin.Server/Program.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.Service.Caller.Authentication.OpenIdConnect;
-
 var builder = WebApplication.CreateBuilder(args);
 
 StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);

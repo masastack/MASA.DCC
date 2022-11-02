6cf92556364c73fc3c2f92fec0a9b1637a108676
yanpengju <yanpengju@masastack.com>
2022-11-02 14:43:13 +0800

    chore: PublishRelease=>PublishReleaseModel

--- src/Contracts/Masa.Dcc.Contracts.Admin/App/Model/PublishReleaseModel.cs
+++ src/Contracts/Masa.Dcc.Contracts.Admin/App/Model/PublishReleaseModel.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Dcc.Contracts.Admin.App.Dtos
+namespace Masa.Dcc.Contracts.Admin.App.Model
 {
-    public class PublishRelease
+    public class PublishReleaseModel
     {
         public string Content { get; set; } = "";
 

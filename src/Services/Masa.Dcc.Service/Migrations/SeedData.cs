dc935f2c8356caf6971792bb1fc919a85eaca3d6
yanpengju <yanpengju@masastack.com>
2022-11-09 17:29:53 +0800

    fix: update public config seed data

--- src/Services/Masa.Dcc.Service/Migrations/SeedData.cs
+++ src/Services/Masa.Dcc.Service/Migrations/SeedData.cs
@@ -24,30 +24,32 @@ namespace Masa.Dcc.Service.Admin.Migrations
 
         private static async Task InitDccDataAsync(DccDbContext context)
         {
-            if (context.Set<Label>().Any())
+            if (!context.Set<Label>().Any())
             {
-                return;
+                var projectTypes = new List<Label>
+                {
+                    new Label("BasicAbility", "BasicAbility", "ProjectType", "项目类型"),
+                    new Label("DataFactory", "DataFactory", "ProjectType", "项目类型"),
+                    new Label("Operator", "Operator", "ProjectType", "项目类型"),
+                    new Label("Other", "其他", "ProjectType", "项目类型"),
+
+                    new Label("Json", "Json", "ConfigObjectFormat", "配置对象类型"),
+                    new Label("Properties", "Properties", "ConfigObjectFormat", "配置对象类型"),
+                    new Label("Raw", "Raw", "ConfigObjectFormat", "配置对象类型"),
+                    new Label("Xml", "Xml", "ConfigObjectFormat", "配置对象类型"),
+                    new Label("Yaml", "Yaml", "ConfigObjectFormat", "配置对象类型"),
+                };
+
+                await context.Set<Label>().AddRangeAsync(projectTypes);
+                await context.SaveChangesAsync();
             }
 
-            var projectTypes = new List<Label>
+            if (!context.Set<PublicConfig>().Any())
             {
-                new Label("BasicAbility", "BasicAbility", "ProjectType", "项目类型"),
-                new Label("DataFactory", "DataFactory", "ProjectType", "项目类型"),
-                new Label("Operator", "Operator", "ProjectType", "项目类型"),
-                new Label("Other", "其他", "ProjectType", "项目类型"),
-
-                new Label("Json", "Json", "ConfigObjectFormat", "配置对象类型"),
-                new Label("Properties", "Properties", "ConfigObjectFormat", "配置对象类型"),
-                new Label("Raw", "Raw", "ConfigObjectFormat", "配置对象类型"),
-                new Label("Xml", "Xml", "ConfigObjectFormat", "配置对象类型"),
-                new Label("Yaml", "Yaml", "ConfigObjectFormat", "配置对象类型"),
-            };
-
-            var publicConfig = new PublicConfig("Public", "public-$Config", "Public config");
-
-            await context.Set<Label>().AddRangeAsync(projectTypes);
-            await context.Set<PublicConfig>().AddAsync(publicConfig);
-            await context.SaveChangesAsync();
+                var publicConfig = new PublicConfig("Public", "public-$Config", "Public config");
+                await context.Set<PublicConfig>().AddAsync(publicConfig);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }

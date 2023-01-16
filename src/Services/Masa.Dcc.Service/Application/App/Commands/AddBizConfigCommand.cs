ee79e084ae2a1be47137ac41164064f6332f870d
yanpengju <yanpengju@masastack.com>
2023-01-16 15:05:44 +0800

    fix: guest

--- src/Services/Masa.Dcc.Service/Application/App/Commands/AddBizConfigCommand.cs
+++ src/Services/Masa.Dcc.Service/Application/App/Commands/AddBizConfigCommand.cs
@@ -3,8 +3,17 @@
 
 namespace Masa.Dcc.Service.Admin.Application.App.Commands
 {
-    public record AddBizConfigCommand(AddObjectConfigDto AddBizConfigDto) : Command
+    //public record AddBizConfigCommand(AddObjectConfigDto AddBizConfigDto) : Command
+    //{
+    //    public BizConfigDto BizConfigDto { get; set; } = new();
+    //}
+
+    //TODO:This is an urgent special treatment,to deal with guest account.
+    //It will be filtered with properties in the future
+    public record AddBizConfigCommand(AddObjectConfigDto AddBizConfigDto) : Query<string?>
     {
         public BizConfigDto BizConfigDto { get; set; } = new();
+
+        public override string? Result { get; set; }
     }
 }

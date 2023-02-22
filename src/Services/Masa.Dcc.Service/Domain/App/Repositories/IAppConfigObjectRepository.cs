e5dc75aaed2c051a17558e23a7ab549627a4cf42
liming <1028139084@qq.com>
2023-02-22 14:04:11 +0800

    fix: The configuration name is the same when you clone the configuration.

--- src/Services/Masa.Dcc.Service/Domain/App/Repositories/IAppConfigObjectRepository.cs
+++ src/Services/Masa.Dcc.Service/Domain/App/Repositories/IAppConfigObjectRepository.cs
@@ -5,6 +5,8 @@ namespace Masa.Dcc.Service.Admin.Domain.App.Repositories
 {
     public interface IAppConfigObjectRepository : IRepository<AppConfigObject>
     {
+        Task<List<AppConfigObject>> GetListByAppIdAsync(int appId);
+
         Task<List<AppConfigObject>> GetListByEnvClusterIdAsync(int envClusterId, int appId);
     }
 }

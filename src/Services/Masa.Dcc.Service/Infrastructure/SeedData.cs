0e9c786c18002571727d928db5552aa4fcf14420
Mayue <mayue8373@live.com>
2023-04-03 19:54:57 +0800

    feat:add menu

--- src/Services/Masa.Dcc.Service/Infrastructure/SeedData.cs
+++ src/Services/Masa.Dcc.Service/Infrastructure/SeedData.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Dcc.Service.Admin.Migrations
+namespace Masa.Dcc.Service.Admin.Infrastructure
 {
     public static class IHostExtensions
     {
@@ -131,7 +131,7 @@ namespace Masa.Dcc.Service.Admin.Migrations
                 { "$public.Sms",GetSms(contentRootPath,masaConfig.Environment) },
                 { "$public.Cdn",GetCdn(contentRootPath,masaConfig.Environment) },
                 { "$public.WhiteListOptions",GetWhiteListOptions(contentRootPath,masaConfig.Environment) },
-                { "$public.i18n.en-us",GetI8nEn(contentRootPath,masaConfig.Environment) },
+                { "$public.i18n.en-us",GetI8nUs(contentRootPath,masaConfig.Environment) },
                 { "$public.i18n.zh-cn",GetI8nCn(contentRootPath,masaConfig.Environment) }
             };
 
@@ -146,15 +146,15 @@ namespace Masa.Dcc.Service.Admin.Migrations
             await context.SaveChangesAsync();
         }
 
-        private static string GetI8nEn(string contentRootPath, string environment)
+        private static string GetI8nUs(string contentRootPath, string environment)
         {
-            var filePath = CombineFilePath(contentRootPath, "$public.i18n.en-us", environment);
+            var filePath = CombineFilePath(contentRootPath, "$public.i18n.en-us.json", environment);
             return File.ReadAllText(filePath);
         }
 
         private static string GetI8nCn(string contentRootPath, string environment)
         {
-            var filePath = CombineFilePath(contentRootPath, "$public.i18n.zh-cn", environment);
+            var filePath = CombineFilePath(contentRootPath, "$public.i18n.zh-cn.json", environment);
             return File.ReadAllText(filePath);
         }
 

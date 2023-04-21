b504feccd5b96e05be62505f7abcbbf4fe930ab7
yanpengju <yanpengju@masastack.com>
2023-04-21 13:26:14 +0800

    fix: fix #549

---
+++ src/Services/Masa.Dcc.Service/Infrastructure/Middleware/CheckUserMiddleware.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Dcc.Service.Admin.Infrastructure.Middleware
+{
+    public class CheckUserMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CheckUserMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IUserContext userContext, IUserSetter userSetter, IMasaStackConfig masaConfig)
+        {
+            var identityUser = userContext.GetUser();
+
+            IDisposable? userSetterHandle = null;
+            if (identityUser == null)
+            {
+                string system = "system";
+                var auditUser = new IdentityUser() { Id = masaConfig.GetDefaultUserId().ToString(), UserName = system };
+                userSetterHandle = userSetter!.Change(auditUser);
+            }
+
+            await _next(context);
+            userSetterHandle?.Dispose();
+        }
+    }
+}

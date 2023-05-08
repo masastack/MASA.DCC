3f9f16267007a481bbc19a0081e076cc233787c1
zhendong.dong <dongzhendong@masastack.com>
2023-05-08 10:40:20 +0800

    remove using

--- src/Services/Masa.Dcc.Service/Infrastructure/Repositories/App/AppConfigObjectRepository.cs
+++ src/Services/Masa.Dcc.Service/Infrastructure/Repositories/App/AppConfigObjectRepository.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Microsoft.AspNetCore.Mvc.Formatters;
-
 namespace Masa.Dcc.Service.Admin.Infrastructure.Repositories.App
 {
     public class AppConfigObjectRepository : Repository<DccDbContext, AppConfigObject>, IAppConfigObjectRepository

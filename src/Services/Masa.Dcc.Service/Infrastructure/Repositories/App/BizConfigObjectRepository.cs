14ee9642314f6d76cfdf3fe338c284bdc4184ba1
zhendong.dong <dongzhendong@masastack.com>
2023-05-09 19:11:11 +0800

    Update BizConfigObjectRepository.cs

--- src/Services/Masa.Dcc.Service/Infrastructure/Repositories/App/BizConfigObjectRepository.cs
+++ src/Services/Masa.Dcc.Service/Infrastructure/Repositories/App/BizConfigObjectRepository.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using System.Globalization;
 namespace Masa.Dcc.Service.Admin.Infrastructure.Repositories.App
 {
     public class BizConfigObjectRepository : Repository<DccDbContext, BizConfigObject>, IBizConfigObjectRepository

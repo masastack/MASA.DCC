181468e699dc83a14772e25eb8d17a7b339fb68d
Mayue <mayue8373@live.com>
2023-05-23 13:41:47 +0800

    feat:update image name

--- power.ps1
+++ power.ps1
@@ -1,4 +1,4 @@
-param($t,$u,$p)
+param($t, $u, $p)
 
 #docker build 
 #Write-Host "Hello.$args"
@@ -6,10 +6,10 @@ Write-Host "Hello $t"
 
 docker login --username=$u registry.cn-hangzhou.aliyuncs.com --password=$p
 
-$ServiceDockerfilePath="./src/Services/Masa.Dcc.Service/Dockerfile"
-$ServiceServerName="masa-dcc-service-admin"
-$WebDockerfilePath="./src/Web/Masa.Dcc.Web.Admin/Masa.Dcc.Web.Admin.Server/Dockerfile"
-$WebServerName="masa-dcc-web-admin-server"
+$ServiceDockerfilePath = "./src/Services/Masa.Dcc.Service/Dockerfile"
+$ServiceServerName = "masa-dcc-service-admin"
+$WebDockerfilePath = "./src/Web/Masa.Dcc.Web.Admin/Masa.Dcc.Web.Admin.Server/Dockerfile"
+$WebServerName = "masa-dcc-web-admin"
 
 docker build -t registry.cn-hangzhou.aliyuncs.com/masastack/${WebServerName}:$t  -f $WebDockerfilePath .
 docker push registry.cn-hangzhou.aliyuncs.com/masastack/${WebServerName}:$t 

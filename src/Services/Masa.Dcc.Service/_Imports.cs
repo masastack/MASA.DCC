fdf95e17956c138457721efc04aa00cdda204b7d
wzh425 <wuzihao425@outlook.com>
2024-08-23 14:22:03 +0800

    🆕 feat: GetPublicConfigAsync

--- src/Services/Masa.Dcc.Service/_Imports.cs
+++ src/Services/Masa.Dcc.Service/_Imports.cs
@@ -15,6 +15,7 @@ global using FluentValidation.Resources;
 global using Mapster;
 global using Masa.BuildingBlocks.Authentication.Identity;
 global using Masa.BuildingBlocks.Caching;
+global using Masa.BuildingBlocks.Configuration;
 global using Masa.BuildingBlocks.Data.UoW;
 global using Masa.BuildingBlocks.Ddd.Domain.Entities.Full;
 global using Masa.BuildingBlocks.Ddd.Domain.Events;

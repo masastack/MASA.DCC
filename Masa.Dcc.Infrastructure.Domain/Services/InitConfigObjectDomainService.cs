2f091a3b8adddb9789be9fe13e581d35bdd02bdc
qinyouzeng <qinyouzeng@masastack.com>
2025-09-03 15:00:02 +0800

    feat: rebuild sqlserver migration file, update init cache

--- Masa.Dcc.Infrastructure.Domain/Services/InitConfigObjectDomainService.cs
+++ Masa.Dcc.Infrastructure.Domain/Services/InitConfigObjectDomainService.cs
@@ -7,26 +7,21 @@ public class InitConfigObjectDomainService(
     IDomainEventBus eventBus,
     IConfigObjectReleaseRepository configObjectReleaseRepository,
     IConfigObjectRepository configObjectRepository,
-    IAppConfigObjectRepository appConfigObjectRepository,
     IPublicConfigRepository publicConfigRepository,
     IMultilevelCacheClient memoryCacheClient,
     IPmClient pmClient,
     IMasaStackConfig masaStackConfig,
+    ILogger<InitConfigObjectDomainService> logger,
     IUnitOfWork unitOfWork) : DomainService(eventBus)
 {
     private readonly IConfigObjectReleaseRepository _configObjectReleaseRepository = configObjectReleaseRepository;
     private readonly IConfigObjectRepository _configObjectRepository = configObjectRepository;
-    private readonly IAppConfigObjectRepository _appConfigObjectRepository = appConfigObjectRepository;
     private readonly IPublicConfigRepository _publicConfigRepository = publicConfigRepository;
     private readonly IMultilevelCacheClient _memoryCacheClient = memoryCacheClient;
     private readonly IPmClient _pmClient = pmClient;
     private readonly IMasaStackConfig _masaStackConfig = masaStackConfig;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
-    private readonly JsonSerializerOptions _jsonSerializerOptions = new()
-    {
-        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-        WriteIndented = true
-    };
+    private readonly ILogger _logger = logger;
 
     private string EncryptContent(string content)
     {
@@ -38,7 +33,9 @@ public class InitConfigObjectDomainService(
 
     private async Task AddConfigObjectReleaseAsync(AddConfigObjectReleaseDto dto)
     {
-        var configObject = (await _configObjectRepository.FindAsync(configObject => configObject.Id == dto.ConfigObjectId)) ?? throw new Exception("Config object does not exist");
+        var configObject = await _configObjectRepository.FindAsync(configObject => configObject.Id == dto.ConfigObjectId);
+        if (configObject == null)
+            throw new Exception($"Config object ConfigObjectId:{dto.ConfigObjectId} does not exist");
 
         configObject.AddContent(configObject.Content, configObject.Content);
         await _configObjectRepository.UpdateAsync(configObject);
@@ -50,79 +47,19 @@ public class InitConfigObjectDomainService(
                configObject.Content);
         await _configObjectReleaseRepository.AddAsync(configObjectRelease);
 
-        var relationConfigObjects = await _configObjectRepository.GetRelationConfigObjectWithReleaseHistoriesAsync(configObject.Id);
-        if (relationConfigObjects != null && relationConfigObjects.Count > 0)
+        //add redis cache
+        var key = $"{dto.EnvironmentName}-{dto.ClusterName}-{dto.Identity}-{configObject.Name}";
+        if (configObject.Encryption)
         {
-            // var allEnvClusters = await _pmClient.ClusterService.GetEnvironmentClustersAsync();
-            var appConfigObjects = await _appConfigObjectRepository.GetListAsync(app => relationConfigObjects.Select(c => c.Id).Contains(app.ConfigObjectId));
-            var apps = await _pmClient.AppService.GetListAsync();
-
-            foreach (var item in appConfigObjects)
-            {
-                // var envCluster = allEnvClusters.FirstOrDefault(c => c.Id == item.EnvironmentClusterId) ?? new();
-                var ClusterName = _masaStackConfig.Cluster;
-                var app = apps.FirstOrDefault(a => a.Id == item.AppId) ?? new();
-                var relationConfigObject = relationConfigObjects.First(c => c.Id == item.ConfigObjectId);
-                var key = $"{_masaStackConfig.Environment}-{_masaStackConfig.Cluster}-{app.Identity}-{relationConfigObject.Name}";
-
-                if (relationConfigObject.FormatLabelCode.ToLower() == "properties")
-                {
-                    var appRelease = relationConfigObject.ConfigObjectRelease.OrderByDescending(c => c.Id).FirstOrDefault();
-                    if (appRelease == null)
-                    {
-                        await _memoryCacheClient.SetAsync(key.ToLower(), new PublishReleaseModel
-                        {
-                            Content = dto.Content,
-                            FormatLabelCode = configObject.FormatLabelCode,
-                            Encryption = configObject.Encryption
-                        });
-                    }
-                    else
-                    {
-                        //compare
-                        var publicContents = JsonSerializer.Deserialize<List<ConfigObjectPropertyContentDto>>(dto.Content) ?? new();
-                        var appContents = JsonSerializer.Deserialize<List<ConfigObjectPropertyContentDto>>(appRelease.Content) ?? new();
-
-                        var exceptContent = publicContents.ExceptBy(appContents.Select(c => c.Key), content => content.Key).ToList();
-                        var content = appContents.Union(exceptContent).ToList();
-
-                        var releaseContent = new PublishReleaseModel
-                        {
-                            Content = JsonSerializer.Serialize(content, _jsonSerializerOptions),
-                            FormatLabelCode = configObject.FormatLabelCode,
-                            Encryption = configObject.Encryption
-                        };
-                        await _memoryCacheClient.SetAsync(key.ToLower(), releaseContent);
-                    }
-                }
-                else
-                {
-                    var releaseContent = new PublishReleaseModel
-                    {
-                        Content = dto.Content,
-                        FormatLabelCode = configObject.FormatLabelCode,
-                        Encryption = configObject.Encryption
-                    };
-                    await _memoryCacheClient.SetAsync(key.ToLower(), releaseContent);
-                }
-            }
+            dto.Content = EncryptContent(dto.Content);
         }
-        else
+        var releaseContent = new PublishReleaseModel
         {
-            //add redis cache
-            var key = $"{dto.EnvironmentName}-{dto.ClusterName}-{dto.Identity}-{configObject.Name}";
-            if (configObject.Encryption)
-            {
-                dto.Content = EncryptContent(dto.Content);
-            }
-            var releaseContent = new PublishReleaseModel
-            {
-                Content = dto.Content,
-                FormatLabelCode = configObject.FormatLabelCode,
-                Encryption = configObject.Encryption
-            };
-            await _memoryCacheClient.SetAsync(key.ToLower(), releaseContent);
-        }
+            Content = dto.Content,
+            FormatLabelCode = configObject.FormatLabelCode,
+            Encryption = configObject.Encryption
+        };
+        await _memoryCacheClient.SetAsync(key.ToLower(), releaseContent);
     }
 
     public async Task InitConfigObjectAsync(
@@ -137,6 +74,11 @@ public class InitConfigObjectDomainService(
         var env = envs.FirstOrDefault(e => e.Name.ToLower() == environmentName.ToLower()) ?? throw new UserFriendlyException("Environment does not exist");
         var clusters = await _pmClient.ClusterService.GetListByEnvIdAsync(env.Id);
         var cluster = clusters.FirstOrDefault(c => c.Name.ToLower() == clusterName.ToLower()) ?? throw new UserFriendlyException("Cluster does not exist");
+
+        var publicConfig = await _publicConfigRepository.FindAsync(publicConfig => publicConfig.Identity == appId);
+        if (publicConfig == null)
+            throw new ArgumentException($"dcc init failed: Identity {appId} is not exists in PublicConfigs");
+
         foreach (var configObject in configObjects)
         {
             var configObjectName = configObject.Key;
@@ -144,24 +86,21 @@ public class InitConfigObjectDomainService(
             if (isEncryption)
                 content = EncryptContent(content);
 
-            var newConfigObject = new ConfigObject(
-                configObjectName,
-                "JSON",
-                configObjectType,
-                content,
-                "{}",
-                encryption: isEncryption);
+            var newConfigObject = await _configObjectRepository.FindAsync(x => x.Name == configObjectName && x.Type == configObjectType);
 
-            var publicConfig = await _publicConfigRepository.FindAsync(publicConfig => publicConfig.Identity == appId);
-            if (publicConfig == null)
-                throw new ArgumentException($"dcc init failed: Identity {appId} is not exists in PublicConfigs");
-
-            newConfigObject.SetConfigObjectType(ConfigObjectType.App);
-            newConfigObject.SetPublicConfigObject(publicConfig.Id, cluster.EnvironmentClusterId);
-
-            //already init success
-            if (await _configObjectRepository.FindAsync(x => x.Name == newConfigObject.Name && x.Type == newConfigObject.Type) == null)
+            if (newConfigObject == null)
             {
+                newConfigObject = new ConfigObject(
+                   configObjectName,
+                   "JSON",
+                   configObjectType,
+                   content,
+                   "{}",
+                   encryption: isEncryption);
+
+                newConfigObject.SetConfigObjectType(ConfigObjectType.App);
+                newConfigObject.SetPublicConfigObject(publicConfig.Id, cluster.EnvironmentClusterId);
+
                 await _configObjectRepository.AddAsync(newConfigObject);
                 await _unitOfWork.SaveChangesAsync();
             }
@@ -170,6 +109,8 @@ public class InitConfigObjectDomainService(
             var redisData = await _memoryCacheClient.GetAsync<PublishReleaseModel?>(key);
             if (redisData != null)
                 continue;
+
+            _logger.LogInformation("InitConfigObjectAsync add redis cache key:{Key}", key);
 
             var releaseModel = new AddConfigObjectReleaseDto
             {
@@ -181,6 +122,7 @@ public class InitConfigObjectDomainService(
                 Identity = appId,
                 Content = configObject.Value
             };
+
             await AddConfigObjectReleaseAsync(releaseModel);
         }
     }
--- src/Services/Masa.Dcc.Service/Infrastructure/SeedData.cs
+++ src/Services/Masa.Dcc.Service/Infrastructure/SeedData.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
+using System.Text.Encodings.Web;
+
 namespace Masa.Dcc.Service.Admin.Infrastructure;
 
 internal static class IHostExtensions
@@ -20,6 +22,9 @@ internal static class IHostExtensions
     {
         var services = builder.Services.BuildServiceProvider().CreateScope().ServiceProvider;
         var context = services.GetRequiredService<DccDbContext>();
+
+        await MigrateAsync(context);
+
         var labelDomainService = services.GetRequiredService<LabelDomainService>();
         var configObjectDomainService = services.GetRequiredService<InitConfigObjectDomainService>();
         var unitOfWork = services.GetRequiredService<IUnitOfWork>();
@@ -33,22 +38,18 @@ internal static class IHostExtensions
         var auditUser = new IdentityUser() { Id = masaConfig.GetDefaultUserId().ToString(), UserName = system };
         var userSetterHandle = userSetter!.Change(auditUser);
 
-        await MigrateAsync(context);
-
         unitOfWork.UseTransaction = false;
+        var configs = GetConfigMap(builder.Services);
 
         await InitDccDataAsync(context, labelDomainService);
-        await InitPublicConfigAsync(context, masaConfig, contentRootPath, pmClient, configObjectDomainService);
 
+        await InitPublicConfigAsync(context, masaConfig, contentRootPath, pmClient, configObjectDomainService, configs);
         userSetterHandle.Dispose();
     }
 
     private static async Task MigrateAsync(DccDbContext context)
     {
-        if ((await context.Database.GetAppliedMigrationsAsync()).Any())
-        {
-            await context.Database.MigrateAsync();
-        }
+        await context.Database.MigrateAsync();
     }
 
     private static async Task InitDccDataAsync(DccDbContext context, LabelDomainService labelDomainService)
@@ -142,13 +143,9 @@ internal static class IHostExtensions
         IMasaStackConfig masaConfig,
         string contentRootPath,
         IPmClient pmClient,
-        InitConfigObjectDomainService configObjectDomainService)
+        InitConfigObjectDomainService configObjectDomainService,
+        Dictionary<string, string> configs)
     {
-        if (await context.Set<PublicConfig>().AnyAsync())
-        {
-            return;
-        }
-
         var environments = await pmClient.EnvironmentService.GetListAsync();
         ArgumentNullException.ThrowIfNull(environments);
         if (environments == null || environments.Count == 0)
@@ -157,24 +154,51 @@ internal static class IHostExtensions
             throw new ArgumentException($"masastack envinoment:{masaConfig.Environment} is not in pm's envinoments: {string.Join(',', environments.Select(env => env.Name))}");
 
         var appid = "public-$Config";
-        foreach (var environment in environments)
+        var publicConfigs = new Dictionary<string, string>() {
+                {"$public.DefaultConfig", JsonSerializer.Serialize(configs, new JsonSerializerOptions{ Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,WriteIndented = true})}
+        };
+        foreach (var (key, value) in dicConfigFiles)
         {
-            var publicConfigs = new Dictionary<string, string>();
-            foreach (var (key, value) in dicConfigFiles)
-            {
-                var filePath = CombineFilePath(contentRootPath, value, environment.Name);
-                publicConfigs.Add(key, await GetFileContentAsync(filePath));
-            }
-            await configObjectDomainService.InitConfigObjectAsync(environment.Name, masaConfig.Cluster, appid, publicConfigs, ConfigObjectType.Public, false);
-
-            var encryptionPublicConfigs = new Dictionary<string, string>
-            {
-                { "$public.Oss",await GetFileContentAsync(CombineFilePath(contentRootPath,"$public.Oss.json",environment.Name)) }
-            };
-            await configObjectDomainService.InitConfigObjectAsync(environment.Name, masaConfig.Cluster, appid, encryptionPublicConfigs, ConfigObjectType.Public, true);
-
-            await context.SaveChangesAsync();
+            var filePath = CombineFilePath(contentRootPath, value, masaConfig.Environment);
+            publicConfigs.Add(key, await GetFileContentAsync(filePath));
         }
+        await configObjectDomainService.InitConfigObjectAsync(masaConfig.Environment, masaConfig.Cluster, appid, publicConfigs, ConfigObjectType.Public, false);
+
+        var encryptionPublicConfigs = new Dictionary<string, string>
+        {
+                { "$public.Oss",await GetFileContentAsync(CombineFilePath(contentRootPath,"$public.Oss.json",masaConfig.Environment)) }
+        };
+        await configObjectDomainService.InitConfigObjectAsync(masaConfig.Environment, masaConfig.Cluster, appid, encryptionPublicConfigs, ConfigObjectType.Public, true);
+        await context.SaveChangesAsync();
+
+    }
+
+    private static Dictionary<string, string> GetConfigMap(IServiceCollection services)
+    {
+        var serviceProvider = services.BuildServiceProvider();
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+        string environment = configuration.GetValue<string>(MasaStackConfigConstant.ENVIRONMENT)!;
+        environment = string.IsNullOrWhiteSpace(environment) ? configuration["ASPNETCORE_ENVIRONMENT"]! : environment;
+
+        var configs = new Dictionary<string, string>()
+        {
+            { MasaStackConfigConstant.VERSION, configuration.GetValue<string>(MasaStackConfigConstant.VERSION)! },
+            { MasaStackConfigConstant.IS_DEMO, configuration.GetValue<bool>(MasaStackConfigConstant.IS_DEMO).ToString() },
+            { MasaStackConfigConstant.DOMAIN_NAME, configuration.GetValue<string>(MasaStackConfigConstant.DOMAIN_NAME)! },
+            { MasaStackConfigConstant.NAMESPACE, configuration.GetValue<string>(MasaStackConfigConstant.NAMESPACE)! },
+            { MasaStackConfigConstant.CLUSTER, configuration.GetValue<string>(MasaStackConfigConstant.CLUSTER)! },
+            { MasaStackConfigConstant.OTLP_URL, configuration.GetValue < string >(MasaStackConfigConstant.OTLP_URL)! },
+            { MasaStackConfigConstant.REDIS, configuration.GetValue<string>(MasaStackConfigConstant.REDIS)! },
+            { MasaStackConfigConstant.CONNECTIONSTRING, configuration.GetValue<string>(MasaStackConfigConstant.CONNECTIONSTRING)! },
+            { MasaStackConfigConstant.MASA_STACK, configuration.GetValue<string>(MasaStackConfigConstant.MASA_STACK)! },
+            { MasaStackConfigConstant.ELASTIC, configuration.GetValue<string>(MasaStackConfigConstant.ELASTIC)! },
+            { MasaStackConfigConstant.ENVIRONMENT, environment },
+            { MasaStackConfigConstant.ADMIN_PWD, configuration.GetValue<string>(MasaStackConfigConstant.ADMIN_PWD)! },
+            { MasaStackConfigConstant.DCC_SECRET, configuration.GetValue<string>(MasaStackConfigConstant.DCC_SECRET)! },
+            { MasaStackConfigConstant.SUFFIX_IDENTITY, configuration.GetValue<string>(MasaStackConfigConstant.SUFFIX_IDENTITY)! }
+        };
+        return configs;
     }
 
     private static async Task<string> GetFileContentAsync(string path)

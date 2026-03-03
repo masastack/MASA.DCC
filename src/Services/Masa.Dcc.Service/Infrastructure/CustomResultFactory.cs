8a542b95c3c42a08b18a522af04cd3ae51ef7d75
qinyouzeng <qinyouzeng@masastack.com>
2026-03-03 17:24:08 +0800

    添加redis链路记录

--- src/Services/Masa.Dcc.Service/Infrastructure/CustomResultFactory.cs
+++ src/Services/Masa.Dcc.Service/Infrastructure/CustomResultFactory.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using FluentValidation.Results;
-using SharpGrip.FluentValidation.AutoValidation.Endpoints.Results;
-
 namespace Masa.Dcc.Service.Admin.Infrastructure;
 
 public class CustomResultFactory : IFluentValidationAutoValidationResultFactory
